cda989fd224f7c587807f0c3bc719b52c4bdd253
laxit-patel <laxxitpatel@gmail.com>
2024-10-13 14:34:24 +0530

    clean slate, added basic gallery and camera

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { IonContent, IonPage, IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
-import { camera as cameraIcon } from 'ionicons/icons';
-import Camera from '../components/Camera';
-import Gallery from '../components/Gallery';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 
-const Home: React.FC = () => {
-  const [showCamera, setShowCamera] = useState<boolean>(false);
-  const [savedPhotos, setSavedPhotos] = useState<{ uri: string; tags?: string }[]>([]);
-
-  const handleCameraOpen = () => {
-    setShowCamera(true);
-  };
-
-  const handlePhotoSaved = (newPhoto: { uri: string; tags?: string }) => {
-    setSavedPhotos((prevPhotos) => [...prevPhotos, newPhoto]);
-    setShowCamera(false);
-  };
-
-  useEffect(() => {
-    // Automatically open the camera when the app launches
-    setShowCamera(true);
-  }, []);
+const Tab2: React.FC = () => {
+  const { deletePhoto, photos, takePhoto } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
 
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <h1>ParcelPix</h1>
-        <Gallery savedPhotos={savedPhotos} />
-
-        {savedPhotos.length === 0 && !showCamera && (
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>No Photos Found</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <img src="path/to/your/placeholder-image.png" alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
-            </IonCardContent>
-          </IonCard>
-        )}
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
 
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton onClick={handleCameraOpen}>
-            <IonIcon icon={cameraIcon} />
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
 
-        {showCamera && (
-          <Camera
-            setSavedPhotos={handlePhotoSaved}
-            savedPhotos={savedPhotos}
-            closeCamera={() => setShowCamera(false)}
-          />
-        )}
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[{
+            text: 'Delete',
+            role: 'destructive',
+            icon: trash,
+            handler: () => {
+              if (photoToDelete) {
+                deletePhoto(photoToDelete);
+                setPhotoToDelete(undefined);
+              }
+            }
+          }, {
+            text: 'Cancel',
+            icon: close,
+            role: 'cancel'
+          }]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
+
+
       </IonContent>
     </IonPage>
   );
 };
 
-export default Home;
+export default Tab2;
