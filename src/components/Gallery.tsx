359b430c204a37e828f78f58cb3a137896ddd370
laxit-patel <laxxitpatel@gmail.com>
2024-10-13 15:04:58 +0530

    added masonry

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
 import { UserPhoto } from '../hooks/usePhotoGallery';
+import './Gallery.css'; // Import the custom CSS for masonry
 
 interface GalleryProps {
     savedPhotos: UserPhoto[];
@@ -8,11 +9,11 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ savedPhotos }) => {
     return (
-        <IonGrid>
-            <IonRow>
+        <IonGrid className="masonry-grid"> {/* Apply the masonry grid class */}
+            <IonRow className="masonry-row">
                 {savedPhotos.map((photo, index) => (
-                    <IonCol size="6" key={index}>
-                        <IonImg src={photo.webviewPath || ''} />
+                    <IonCol size="6" key={index} className="masonry-col"> {/* Apply masonry column styling */}
+                        <IonImg src={photo.webviewPath || ''} className="masonry-img" /> {/* Apply custom image styling */}
                     </IonCol>
                 ))}
             </IonRow>
